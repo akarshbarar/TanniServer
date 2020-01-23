876f433466eccf0993ec6af6ea36cfcebaaf9b6b
Akarsh Barar <barar.akarsh8@gmail.com>
2020-01-23 18:04:25 +0530

    ADDED

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 
     console.log("Listening on port %s...", server.address().port);
 });
